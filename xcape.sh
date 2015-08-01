4d2468a56f3772781b7f59f1ae5f6d3f954bd0f6
l <l@jusss.org>
2015-08-01 12:38:23 +0000

    fvwm2

---
+++ xcape.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+killall xcape
+# xcape -e 'Control_L=Escape;Meta_L=Tab'
+# because fvwm2 only use C-Tab to switch window, so don't change Tab to Meta_L in  xmodmap, and don't change Tab on xcape
+xcape -e 'Control_L=Escape'
