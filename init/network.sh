5672c09bee85d658b7f29f0e2736770ce35b74a5
l <l@jusss.org>
2015-08-03 11:42:07 +0000

    use ip to instead of dhcpcd

--- init/network.sh
+++ init/network.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 killall wpa_supplicant > /dev/null 2>&1
-sudo killall dhcpcd > /dev/null 2>&1
+sleep 2
 wpa_supplicant -iwlp3s0 -c ~/networks/home -Dwext -d >> ~/lab/log 2>&1 &
 sleep 2
-sudo dhcpcd -4 wlp3s0 >> ~/lab/log 2>&1 & 
+# ip address, subnet mask, broadcast address, device interface
+sudo ip addr add 192.168.1.254/24 broadcast 192.168.1.255 dev wlp3s0
+# route address
+sudo ip route add default via 192.168.1.1
 # for dnsmasq
-#sleep 10
-#echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf >> ~/lab/log 2>&1 &
+#echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf
 
 
