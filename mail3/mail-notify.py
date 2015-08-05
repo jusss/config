8ff45a4c80cc6b7f34fe23bc4661cd548d77a0ec
l <l@jusss.org>
2015-08-05 14:00:03 +0000

    update before check

--- mail3/mail-notify.py
+++ mail3/mail-notify.py
@@ -8,7 +8,7 @@
 ### 取消gnus的每隔5分钟的时间检查
 
 """
-1. 当有新未读邮件并未在其它客户端下载，声音提醒并offlineimap下载，然后每隔2小时判断是否已读，当未读时，声音提醒并offlineimap同步一次
+1. 当有新未读邮件并未在其它客户端下载，声音提醒并offlineimap下载，然后下载2小时后offlineimap同步一次把已读的状态同步上去，2小时10分钟后判断是否有未读邮件，声音提醒并offlineimap同步一次,并把计时置零
 2. 如果根据邮件ID来判断新邮件的话，还得有个ID表文件，或者判断ID个数，这不是很好，所以用RECENT来判断新邮件，但如果在别的客户端上已下新邮件，则这面就不会有RECENT了
    所以如果在别的客户端上已收取新邮件后，这里就不再提醒和offlineimap下载
 3. 之所以不开offlineimap主动每10分钟下载一次，是怕当在下载过程中会收到新邮件再开offlineimap下载，这样2个offlineimap会冲突，但可以开offlineimap前先检查是否有offlinimap进程来避免
@@ -19,7 +19,7 @@
 import socket, ssl, os, io, time, sys
 
 address=''
-port=
+port=993
 user=''
 password=''
 encoding='utf-8'
@@ -62,28 +62,29 @@ while True:
     if recv_msg.find("RECENT") > 0:
         if int(recv_msg[0:recv_msg.find("RECENT")-1][::-1][0:recv_msg[0:recv_msg.find("RECENT")-1][::-1].find(" ")][::-1]) > 0:
             # use psutil module can detect if offlineimap process exist or not
-            detect_id = os.popen("pidof -x offlineimap")
-            if not detect_id.read():
+            if not os.popen("pidof -x offlineimap").read():
                 os.system("offlineimap >/dev/null 2>&1 &")
             os.system("mplayer -noconsolecontrols -really-quiet /home/jusss/sounds/new-email.mp3 2>/dev/null &")
             latest_recent_time=time.time()
 
     if time.time() - latest_recent_time > 7200:
-        latest_recent_time=time.time()
-        ssl_socket.write('done\r\n'.encode(encoding))
-        recv_msg=ssl_socket.read().decode(encoding)[:-2]
-        print(recv_msg)
-        ssl_socket.write('a_tag status inbox (unseen)\r\n'.encode(encoding))
-        recv_msg=ssl_socket.read().decode(encoding)[:-2]
-        print(recv_msg)
-        ssl_socket.write('a_tag idle\r\n'.encode(encoding))
+        if not os.popen("pidof -x offlineimap").read():
+            os.system("offlineimap >/dev/null 2>&1 &")
+        if time.time() - latest_recent_time > 7800:
+            latest_recent_time=time.time()
+            ssl_socket.write('done\r\n'.encode(encoding))
+            recv_msg=ssl_socket.read().decode(encoding)[:-2]
+            print(recv_msg)
+            ssl_socket.write('a_tag status inbox (unseen)\r\n'.encode(encoding))
+            recv_msg=ssl_socket.read().decode(encoding)[:-2]
+            print(recv_msg)
+            ssl_socket.write('a_tag idle\r\n'.encode(encoding))
         
-        if recv_msg[recv_msg.find('UNSEEN')+7] != '0':
-            os.system("mplayer -noconsolecontrols -really-quiet /home/jusss/sounds/new-email.mp3 2>/dev/null &")
-            detect_id = os.popen("pidof -x offlineimap")
-            if not detect_id.read():
-                os.system("offlineimap >/dev/null 2>&1 &")
-
+            if recv_msg[recv_msg.find('UNSEEN')+7] != '0':
+                os.system("mplayer -noconsolecontrols -really-quiet /home/jusss/sounds/new-email.mp3 2>/dev/null &")
+                if not os.popen("pidof -x offlineimap").read():
+                    os.system("offlineimap >/dev/null 2>&1 &")
+            
 #    ssl_socket.write('a_tag status inbox (unseen)\r\n'.encode(encoding))
 #    * STATUS "inbox" (UNSEEN 0)
 #    idle need to be end with 'done'
