85474c6ad694e6c8456b2724011448d0f1b55ded
l <l@jusss.org>
2016-12-08 17:09:28 +0000

    add ip flush

--- init/network.sh
+++ init/network.sh
@@ -4,13 +4,15 @@ sleep 1
 # wpa_supplicant -iwlp3s0 -c ~/networks/home -Dwext -d >> ~/lab/log 2>&1 &
 wpa_supplicant -iwlp3s0 -c ~/networks/home -Dnl80211 > /dev/null 2>&1 &
 sleep 2
+# man ip-address like man git-add
+ip address flush dev wlp3s0
 # ip address, subnet mask, broadcast address, device interface
 ip addr add 192.168.2.254/24 broadcast 192.168.2.255 dev wlp3s0
 # route address
 ip route add default via 192.168.2.1
 # for dnsmasq
 #echo "nameserver 127.0.0.1" | sudo tee /etc/resolv.conf
-systemctl stop dnsmasq
+systemctl start dnsmasq
 
 # sudo python /home/jusss/lab/part-ii-dns-relay.py
 
