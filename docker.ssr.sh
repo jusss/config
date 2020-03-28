099d764367313afcd2fead5fd923ca1b97e80005
John <ifvwm1@gmail.com>
2020-03-29 03:30:04 +0800

    docker ssr

--- docker.ssr.sh
+++ docker.ssr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 1775:1775 -p 1775:1775/udp --env SERVER_PORT=1775 --env PASSWORD=x --env METHOD=chacha20 --env PROTOCOL=origin --env OBFS=http_simple --env PROTOCOLPARAM=104114:m9Rz7o -itd breakwa11/shadowsocksr
+docker run -p 22323:51348 -p 22323:51348/udp --env SERVER_PORT=51348 --env PASSWORD=w --env METHOD=chacha20 --env PROTOCOL=origin --env OBFS=http_simple --env PROTOCOLPARAM=104114:m9Rz7o -itd breakwa11/shadowsocksr
